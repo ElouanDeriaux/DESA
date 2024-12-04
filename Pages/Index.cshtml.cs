830e851ae982f26a567391a7bd60d66bc5e9a9f8
ElouanDeriaux <elouanderiaux@gmail.com>
2024-12-04 21:15:21 +0100

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+// Pages/Index.cshtml.cs
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace webapp_sam_lab13.Pages;
-
-public class IndexModel : PageModel
+namespace webapp_sam_lab13.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly ILogger<IndexModel> _logger;
+        private readonly BlobService _blobService;
 
-    public void OnGet()
-    {
+        // Propriété pour lier le fichier uploadé
+        [BindProperty]
+        public IFormFile File { get; set; }
 
+        // Propriété pour afficher l'URL du fichier uploadé
+        public string FileUrl { get; set; }
+
+        // Constructeur pour injecter BlobService
+        public IndexModel(ILogger<IndexModel> logger, BlobService blobService)
+        {
+            _logger = logger;
+            _blobService = blobService;
+        }
+
+        // Action qui gère l'upload du fichier
+        public async Task<IActionResult> OnPostUploadAsync()
+        {
+            if (File != null && File.Length > 0)
+            {
+                using (var stream = File.OpenReadStream())
+                {
+                    var fileName = Path.GetFileName(File.FileName);
+                    // Appel du service BlobService pour uploader le fichier
+                    FileUrl = await _blobService.UploadFileAsync(stream, fileName);
+                }
+            }
+
+            // Retourner la même page avec l'URL du fichier uploadé
+            return Page();
+        }
+
+        public void OnGet()
+        {
+        }
     }
 }
